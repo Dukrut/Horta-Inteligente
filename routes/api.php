e90e4df62a49474baae1f68dfa5f02af4ed05ed2
Alan Neves <alaneves0801@gmail.com>
2020-09-18 21:42:32 -0300

    Criando middleware e rota logout

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 
 Route::post('auth/login', [Controllers\Api\AuthController::class, 'login']);
-Route::get('/users', [Controllers\Api\UserController::class, 'index']);
+
+Route::group(['middleware' => ['apiJwt']], function () {
+    Route::post('/auth/logout', [Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/users', [Controllers\Api\UserController::class, 'index']);
+});
